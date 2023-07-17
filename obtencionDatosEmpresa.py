25d949e795dd7b70834dfeb22017bd6d932104d5
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-07-17 05:55:26 -0600

    Obtención de datos en formato de HTML

---
+++ obtencionDatosEmpresa.py
@@ -0,0 +1,52 @@
+#pip install beautifulsoup4
+#pip install requests
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def scrape_and_save_html(url):
+    # Making a GET request
+    r = requests.get(url)
+
+    # check status code for response received
+    # success code - 200
+    print(r)
+
+    # Parse HTML content with BeautifulSoup
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    # Find the table with class "genTbl reportTbl"
+    table = soup.find('table', class_='genTbl reportTbl')
+
+    if table:
+        # Extract filename from URL
+        filename = url.split('/')[-1]
+
+        # Save table HTML to a file
+        with open(filename + '.html', 'w', encoding='utf-8') as file:
+            file.write(str(table))
+        print(f"Table HTML content saved to '{filename}.html' file.")
+    else:
+        print("Table not found in the HTML.")
+
+
+# Abrir el archivo JSON
+with open('balance-sheet.json', 'r') as file:
+    data = json.load(file)
+
+with open('income-statement.json', 'r') as file:
+    data_two = json.load(file)
+
+# Acceder a los valores dentro del JSON
+urls = data['href_attributes']
+urls_two = data_two['href_attributes']
+
+# Imprimir los valores obtenidos
+for url in urls:
+    scrape_and_save_html(url)
+
+for url_two in urls_two:
+    scrape_and_save_html(url_two)
+
+
---
+++ selectorMercados.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def get_href_from_urls(urls):
+    href_list = []
+
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        elements = soup.find_all(class_='inv-link bold datatable_cell--name__link__2xqgx')
+        
+        for element in elements:
+            href = element.get('href')
+            if href:
+                href_list.append(href)
+    
+    return href_list
+
+# Lista de URLs para acceder a mercados concretos
+urls = [
+    # 'https://mx.investing.com/equities/americas',
+    'https://mx.investing.com/equities/mexico',
+    # 'https://mx.investing.com/equities/canada',
+    # 'https://mx.investing.com/equities/argentina',
+    # 'https://mx.investing.com/equities/brazil',
+    # 'https://mx.investing.com/equities/spain',
+    # 'https://mx.investing.com/equities/chile',
+    # 'https://mx.investing.com/equities/germany',
+    # 'https://mx.investing.com/equities/switzerland'
+]
+
+# Obtener los atributos href
+href_attributes = get_href_from_urls(urls)
+
+# Agregar texto al final de cada elemento de href_attributes
+modified_attributes = ['https://mx.investing.com' + href + '-income-statement' for href in href_attributes]
+modified_attributes_two = ['https://mx.investing.com' + href + '-balance-sheet' for href in href_attributes]
+
+# Guardar los resultados en un archivo JSON
+data = {'href_attributes': modified_attributes}
+with open('datos/income-statement.json', 'w') as file:
+    json.dump(data, file)
+
+data_two = {'href_attributes': modified_attributes_two}
+with open('datos/balance-sheet.json', 'w') as file:
+    json.dump(data_two, file)
