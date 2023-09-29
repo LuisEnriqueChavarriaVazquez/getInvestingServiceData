0d1d0ce2cd9789e4be5d6aa781b62d45a4273455
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-09-29 06:58:40 -0600

    16 nuevos mercados y JSON con el pais de cada empresa

--- selectorMercados.py
+++ selectorMercados.py
@@ -3,6 +3,35 @@ from bs4 import BeautifulSoup
 import json
 import subprocess
 
+#Funcion para generar un JSON con el nombre de la empresa + el pais. 
+#Esto es para agregar banderas al buscador de HTML
+def get_company_info_from_urls(urls):
+    company_info_list = []
+
+    for url in urls:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        elements = soup.find_all(class_='inv-link bold datatable_cell--name__link__2xqgx')
+        
+        for element in elements:
+            href = element.get('href')
+            if href:
+                # Obtener el nombre de la empresa y el país
+                #name = element.text.strip() Nombre sin guiones
+                name = href.split('/')[2]
+                country = url.split('/')[-1]
+
+                company_info = {
+                    'name': name,
+                    'country': country
+                }
+                
+                company_info_list.append(company_info)
+    
+    return company_info_list #Solo retorna el objeto que se guarda en datos/company-info.js
+
+#Funcion para obtener solamente el nombre de la empresa en una lista
 def get_href_from_urls(urls):
     href_list = []
 
@@ -28,9 +57,27 @@ urls = [
     'https://mx.investing.com/equities/brazil',
     'https://mx.investing.com/equities/spain',
     'https://mx.investing.com/equities/chile',
-    # 'https://mx.investing.com/equities/germany',
-    # 'https://mx.investing.com/equities/switzerland'
+    'https://mx.investing.com/equities/germany',
+    'https://mx.investing.com/equities/switzerland',
+    'https://mx.investing.com/equities/venezuela',
+    'https://mx.investing.com/equities/peru',
+    'https://mx.investing.com/equities/colombia',
+    'https://mx.investing.com/equities/china',
+    'https://mx.investing.com/equities/india',
+    'https://mx.investing.com/equities/russia',
+    'https://mx.investing.com/equities/japan'
 ]
+
+##CODIGO PARA LA FUNCION get_company_info_from_urls
+
+# Obtener información de la empresa, incluyendo nombre y país
+company_info_list = get_company_info_from_urls(urls)
+
+# Guardar los resultados en un archivo JSON
+with open('datos/company-info.json', 'w') as file:
+    json.dump(company_info_list, file)
+
+##CODIGO PARA LA FUNCION get_href_from_urls
 
 # Obtener los atributos href
 href_attributes = get_href_from_urls(urls)
@@ -48,12 +95,8 @@ data_two = {'href_attributes': modified_attributes_two}
 with open('datos/balance-sheet.json', 'w') as file:
     json.dump(data_two, file)
 
-
-#Corremos el programa para ejecutar la creación de words/json
-# Ejecutar el programa de Python
+# Correr el resto del programa
 subprocess.run(['python', 'obtencionDatosEmpresa.py'])
 subprocess.run(['python', 'obtencionNombresEmpresas.py'])
 subprocess.run(['python', 'obtenecionNombresSolos.py'])
 subprocess.run(['python', 'htmlToJsonConverterAll.py'])
-
-
